67fca930872f174cfbca1f73e935ebe117438cd7
Wilfried M Majaliwa <wilfriedmajaliwa@gmail.com>
2025-01-25 15:05:02 +0300

    Dynamic path segments & captured values

--- challenges/models.py
+++ challenges/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
 # Create your models here.
+months = {
+    "January",
+    "February",
+    "March",
+    "April",
+    "May",
+    "June",
+    "July",
+    "August",
+    "September",
+    "October",
+    "November",
+    "December",
+}
--- challenges/views.py
+++ challenges/views.py
@@ -1,12 +1,13 @@
-from django.http import HttpResponse
-from django.shortcuts import render
+from django.http import HttpResponse, HttpResponseNotFound
+
+from . import models
 
 
 # Create your views here.
 
-def january(request):
-    return HttpResponse("<h1>Hello There!</h1>")
-
-
-def february(request):
-    return HttpResponse("It's not yet february")
+def monthly_challenge(request, month):
+    for m in models.months:
+        if m.casefold() == month.casefold():
+            return HttpResponse(f"<h1>We are in {m}</h1>")
+    else:
+        return HttpResponseNotFound("Error:")
