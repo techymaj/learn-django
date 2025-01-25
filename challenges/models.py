bec3aac895cee5b3407ca211579baba1e31562c8
Wilfried M Majaliwa <wilfriedmajaliwa@gmail.com>
2025-01-25 15:42:27 +0300

    Path converters

--- challenges/models.py
+++ challenges/models.py
@@ -2,16 +2,16 @@ from django.db import models
 
 # Create your models here.
 months = {
-    "January",
-    "February",
-    "March",
-    "April",
-    "May",
-    "June",
-    "July",
-    "August",
-    "September",
-    "October",
-    "November",
-    "December",
+    "January": 31,
+    "February": 28,
+    "March": 30,
+    "April": 31,
+    "May": 30,
+    "June": 31,
+    "July": 31,
+    "August": 30,
+    "September": 31,
+    "October": 30,
+    "November": 31,
+    "December": 31,
 }
--- challenges/views.py
+++ challenges/views.py
@@ -5,9 +5,14 @@ from . import models
 
 # Create your views here.
 
-def monthly_challenge(request, month):
-    for m in models.months:
+def monthly_challenge(request, month, day):
+    for m, days in models.months.items():
         if m.casefold() == month.casefold():
-            return HttpResponse(f"<h1>We are in {m}</h1>")
+            return HttpResponse(
+                f"""
+                    <h1>We are in {m}</h1>
+                    <p>It has {days} days</p>
+                    <p>Your chosen date is <strong>{day}/{month}/2025</strong></p>
+                """)
     else:
         return HttpResponseNotFound("Error:")
