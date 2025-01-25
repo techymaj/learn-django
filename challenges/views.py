31c05fd0aa460095072dea893e4c7d2c166ddfb4
Wilfried M Majaliwa <wilfriedmajaliwa@gmail.com>
2025-01-25 14:26:23 +0300

    Intro to views and urls

--- challenges/views.py
+++ challenges/views.py
@@ -1,3 +1,12 @@
+from django.http import HttpResponse
 from django.shortcuts import render
 
+
 # Create your views here.
+
+def january(request):
+    return HttpResponse("<h1>Hello There!</h1>")
+
+
+def february(request):
+    return HttpResponse("It's not yet february")
