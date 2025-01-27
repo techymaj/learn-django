adae08bb00c2c11361bd973d7706205222031770
Wilfried M Majaliwa <wilfriedmajaliwa@gmail.com>
2025-01-27 08:09:10 +0300

    Template partials

--- challenges/urls.py
+++ challenges/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("", views.index),
+    path("", views.index, name="challenge"),
     path(
         "<str:month>",
         views.monthly_challenge,
